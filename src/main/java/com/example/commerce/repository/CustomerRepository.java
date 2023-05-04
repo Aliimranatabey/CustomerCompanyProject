6e73fbb5465b11c0075dcd004f88aa0e5f1780ba
Aliimranatabey <aliimranatabey@gmail.com>
2023-05-04 12:54:06 +0300

    restrepository added

--- src/main/java/com/example/commerce/repository/CustomerRepository.java
+++ src/main/java/com/example/commerce/repository/CustomerRepository.java
@@ -2,9 +2,11 @@ package com.example.commerce.repository;
 import com.example.commerce.entity.Customer;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+@RepositoryRestResource(path = "customer")
 public interface CustomerRepository extends JpaRepository<Customer,UUID>{
     
 }
+
+
