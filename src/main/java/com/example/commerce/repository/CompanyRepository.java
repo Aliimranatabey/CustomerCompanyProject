d40f27a105886d5c58b2a0169591b7eee6ade4d2
Aliimranatabey <aliimranatabey@gmail.com>
2023-05-17 22:45:57 +0300

    try restrepository

--- src/main/java/com/example/commerce/repository/CompanyRepository.java
+++ src/main/java/com/example/commerce/repository/CompanyRepository.java
@@ -1,4 +1,5 @@
 package com.example.commerce.repository;
+import java.io.File;
 import java.util.UUID;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
@@ -11,8 +12,7 @@ import com.example.commerce.entity.Company;
 @RepositoryRestResource(path = "company")
 public interface CompanyRepository extends JpaRepository<Company, UUID> {
 
-    @Query("SELECT c FROM Company c WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', CONCAT(:name, '%'))) AND LOWER(c.url) LIKE LOWER(CONCAT('%', CONCAT(:url, '%')))")
-    Page<Company> findByCompany(@Param("name") String name, @Param("url") String url, Pageable pageable);
+    @Query("SELECT c from Company c WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', CONCAT(:name, ('%')))) AND LOWER(c.url) LIKE LOWER(CONCAT('%', CONCAT(:url, concat('%')))) AND LOWER(c.image) LIKE LOWER(CONCAT('%', CONCAT(:image, concat('%'))))")
+    Page<Company> findByCompany(@Param("name") String name, @Param("url") String url,@Param("image") File image, Pageable pageable);
+
 }
-
-
--- src/main/java/com/example/commerce/repository/CustomerRepository.java
+++ src/main/java/com/example/commerce/repository/CustomerRepository.java
@@ -1,9 +1,9 @@
 package com.example.commerce.repository;
 import com.example.commerce.entity.Company;
 import com.example.commerce.entity.Customer;
+import com.example.commerce.entity.Gender;
 import java.util.List;
 import java.util.UUID;
-
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,15 +14,15 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(path = "customer")
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
-    @Query("SELECT c FROM Customer c LEFT OUTER JOIN c.company sc WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', CONCAT(:name, '%'))) AND "
-            + "LOWER(c.surname) LIKE LOWER(CONCAT('%', CONCAT(:surname, '%'))) AND "
-            + "LOWER(c.email) LIKE LOWER(CONCAT('%', CONCAT(:email, '%'))) AND "
-            + "LOWER(c.password) LIKE LOWER(CONCAT('%', CONCAT(:password, '%'))) AND "
-            + "(:company IS NULL OR LOWER(sc.name) LIKE LOWER(CONCAT('%', CONCAT(:company, '%'))))")
+    @Query("SELECT c FROM Customer c LEFT OUTER JOIN c.company sc WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', CONCAT(:name, concat('%')))) AND "
+            + "LOWER(c.surname) LIKE LOWER(CONCAT('%', CONCAT(:surname, concat('%')))) AND "
+            + "LOWER(c.email) LIKE LOWER(CONCAT('%', CONCAT(:email, concat('%')))) AND "
+            + "LOWER(c.password) LIKE LOWER(CONCAT('%', CONCAT(:password, concat('%')))) AND "
+            + "LOWER(c.gender) LIKE LOWER(CONCAT('%', CONCAT(:gender, concat('%')))) AND "
+            + "LOWER(c.birthday) LIKE LOWER(CONCAT('%', CONCAT(:birthday, concat('%')))) AND "
+            + "(:company='' OR LOWER(sc.name) LIKE LOWER(CONCAT('%', CONCAT(:company, concat('%')))))")
     List<Customer> findAllSearch(@Param("name") String name, @Param("surname") String surname,
             @Param("email") String email, @Param("password") String password,
             @Param("company") Company company);
-        Page<Customer> findByCustomer(@Param("name") String name, @Param("surname") String surname,@Param("email") String email,@Param("password") String password, Pageable pageable);
+        Page<Customer> findByCustomer(@Param("name") String name, @Param("surname") String surname,@Param("email") String email,@Param("password") String password, @Param("gender") Gender gender, Pageable pageable);
 }
-
-
